ba15e0c9096b4c6da4e0da94ca98147841a747df
Wojtek Waliś <w.walis367@gmail.com>
2019-08-11 13:31:19 +0200

    O co tu chodzi

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        List<float> ratings = new List<float>();
+
+        public void AddRating( float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float var1 = 0, avg = 0;
+            foreach (var rating in ratings)
+            {
+                var1 += rating;
+            }
+            avg = var1 / ratings.Count();
+
+            return avg;
+        }
+        public float GiveMax()
+        {
+            return ratings.Max();
+        }
+        public float GiveMin()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ StudentDiary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            //diary.AddRating(5);
+            //diary.AddRating(8.5f);
+            //diary.AddRating(4.7f);
+
+            //float avg = diary.CalculateAverage();
+            //float max = diary.GiveMax();
+            //float min = diary.GiveMin();
+
+            for (; ;)
+            {
+                Console.WriteLine("Podaj ocene : ");
+                float rating;
+                bool result = float.TryParse((Console.ReadLine()),out rating);
+               
+                if (result)
+                {
+                    if (rating < 1 || rating > 10)
+                    {
+                        Console.WriteLine("Nie poprawna liczba");
+                        Console.WriteLine("srednia twoich ocen to " + diary.CalculateAverage());
+                        Console.WriteLine("Najwyzsza ocena to " + diary.GiveMax());
+                        Console.WriteLine("Najnizsza ocena " + diary.GiveMin());
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        diary.AddRating(rating);
+                    }
+                }
+            }
+            
+        }
+    }
+}
